78774613750c6f39e9e79de104e83d7ad87dfac4
grace moon <hi.gracemoon@gmail.com>
2016-12-13 23:13:45 -0500

    post css and seeds fix

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -11,14 +11,16 @@ const Post = (props) =>  {
 		 	<div id="container">
 		 	  <link href="https://fonts.googleapis.com/css?family=Amatica+SC:400,700|Bentham|Comfortaa:300,400,700|Fredoka+One|Gruppo|Judson:400,400i,700|Life+Savers:400,700|Open+Sans+Condensed:300,300i,700|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i|Prata|Roboto+Slab:100,300,400,700|Rufina:400,700|Suranna" rel="stylesheet" />
 
-		 	<div className="post-cont">
-		 	<h1>{`${props.post_title}`}</h1>
-			<img src={`${props.post_img}`} width="300"/>
-		 	<p>{`${props.post_content}`}</p>
 
-			<br/>
+				<div className="post-cont">		 	 
+				 	<div className="post-item-cont">
+				 	<h1 className="titleh1">{`${props.post_title}`}</h1>
+					<img className="imgh1" src={`${props.post_img}`} width="300"/>
+				 	<p className="contenth1">{`${props.post_content}`}</p>
+					<br/>
+					</div>
+				</div>
 
-			</div>
 		 	</div>
 		 );
 	 
