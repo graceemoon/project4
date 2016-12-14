cb941c9808da6f967da91af71bedbded3c4c014a
grace moon <hi.gracemoon@gmail.com>
2016-12-13 23:59:47 -0500

    new post button

--- src/components/Start/Start.jsx
+++ src/components/Start/Start.jsx
@@ -14,17 +14,20 @@ console.log(props)
 		 return (
 		 	<div id="container">
 		 		<link href="https://fonts.googleapis.com/css?family=Amatica+SC:400,700|Bentham|Comfortaa:300,400,700|Fredoka+One|Gruppo|Judson:400,400i,700|Life+Savers:400,700|Open+Sans+Condensed:300,300i,700|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i|Prata|Roboto+Slab:100,300,400,700|Rufina:400,700|Suranna" rel="stylesheet" />
-
+				<Header />
 			 	<div className="start-cont">
-				 	<Header />
-				 	
-				 	<div>Start Page</div>
+
+
+				 	<div className="new-cont">
 				 	<Link className="newpost-butt" to="/newpost">New Post</Link>
+				 	</div>
+
 				 	<PostContainer 
 				 	posts={props.state.posts}/>
 
-				 	<Footer />
+
 				</div>
+				<Footer />
 		 	</div>
 		 );
 };
